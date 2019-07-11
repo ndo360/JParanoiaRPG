3db27e6986e2e7aeb54767a771c92ce13e2aa1ef
Zachary Litzinger <zachary.litzinger.93@gmail.com>
2019-07-11 12:40:37 -0400

    Added UTF-8 support for logwriter

--- src/main/java/util/LogWriter.java
+++ src/main/java/util/LogWriter.java
@@ -1,24 +1,43 @@
 package util;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 
 public class LogWriter {
-    PrintWriter out;
+	Writer fstream = null;
+	BufferedWriter out = null;
 
     public LogWriter( String paramString ) {
-        try {
-            this.out = new PrintWriter( new FileWriter( paramString, true ), true );
-        } catch ( IOException localIOException ) {
-            localIOException.printStackTrace();
-        }
+    	
+    	    try {
+				fstream = new OutputStreamWriter(new FileOutputStream(paramString), StandardCharsets.UTF_8);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
     }
 
     public void logEntry( String paramString ) {
-        this.out.println( paramString );
+    	try {
+			this.out.write(paramString);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     public void close() {
-        this.out.close();
+        try {
+			this.out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
